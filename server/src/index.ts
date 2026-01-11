ce5dccffa0459441e85a7dabbbdbd0cfb1cc913e
SumanSaha <sumansahaweb.dev@gmail.com>
2026-01-11 20:49:21 +0530

    `Added CORS middleware to handle OPTIONS requests globally-IV`

--- server/src/index.ts
+++ server/src/index.ts
@@ -59,6 +59,26 @@ app.use(express.json());
 // Handle OPTIONS requests globally
 // app.options('*', cors(corsOptions));
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', req.headers.origin || '*');
+    res.header(
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    );
+    res.header(
+      'Access-Control-Allow-Methods',
+      'GET, POST, PUT, DELETE, OPTIONS'
+    );
+    res.header('Access-Control-Allow-Credentials', 'true');
+  
+    if (req.method === 'OPTIONS') {
+      return res.sendStatus(200);
+    }
+  
+    next();
+  });
+  
+
 // Routes
 app.use('/api/products', productRoutes);
 app.use('/api/categories', categoryRoutes);
